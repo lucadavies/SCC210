90db4ce8f3da1229604524c270227dfc0736d123
ArmenZz <s.savu@lancaster.ac.uk>
2019-01-21 17:51:32 +0000

    Update Map.java
    Added  enum type for maps, set grid to Tile type, switch case now sets the background and boundaries for each map to their respective biome images, added getter for any tile

--- Map.java
+++ Map.java
@@ -3,53 +3,94 @@
 *Class for the map which is displayed to the user
 *
 */
-import java.util.Enumeration;
-import java.util.Vector;
 
 public class Map extends AnimatedSprite {
 
-    int seed;
-    int grid[17][17];
-    Enumeration tiles;
+    mapType seed;
+	Tile grid = new Tile[17][17];
+	
+    //enum type for the maps public so u can use it outside of this
+    public enum mapType 
+	{
+		FARM, 
+		FOREST,
+		RIVER,
+		CAVE, 
+		PLANET,
+		SHIP 
+	}
 
-    Vector tiles1 = new Vector();
-    Vector tiles2 = new Vector();
-    Vector tiles3 = new Vector();
-    Vector tiles4 = new Vector();
-    Vector tiles5 = new Vector();
-    Vector tiles6 = new Vector();
-
-
-    public Map(int seed)
+	//constructor which provides seed to generate a specific map
+    public Map(mapType seed)
     {
         this.seed=seed;
-    }
-
-    public void generateMap()
-    {
         switch (seed)
         {
-            case 1:
-                tiles = tiles1.elements;
-            case 2:
-                tiles = tiles2.elements;
-            case 3:
-                tiles = tiles3.elements;
-            case 4:
-                tiles = tiles4.elements;
-            case 5:
-                tiles = tiles5.elements;
-            case 6:
-                tiles = tiles6.elements;
+            case FARM:
+                this.setBackground("farm_background.png");
+                this.setBoundaries("bush.png")
+                break;
+            case FOREST:
+                this.setBackground("forest_background.png");
+                this.setBoundaries("bush.png")
+                break;
+            case RIVER:
+                this.setBackground("river_background.png");
+                this.setBoundariesRiver("bush.png")
+                break;
+            case CAVE:
+                this.setBackground("cave_background.png");
+                this.setBoundariesCave("bush.png")
+                break;
+            case PLANET:
+                this.setBackground("planet_background.png");
+                this.setBoundaries("bush.png")
+                break;
+            case SHIP:
+                this.setBackground("ship_background.png");
+                this.setBoundaries("bush.png")
+                break;
         }
-
+    }
+    //sets all the tiles to one single image
+	private void setBackground(String backgroundTile)
+	{
+		for(int i=0; i<17;i++)
+			for (int j=0; j<17;j++)
+				grid[i][j]= new Tile(backgroundTile);
+	}
+	//sets the boundaries of the map to one single image
+    private void setBoundaries(String image)
+    {
         for(int i=0; i<17;i++)
             for (int j=0; j<17;j++)
-                grid[i][j]=tiles.nextElement();
+                if (i==0 || j==16 || j==0 || j==16 )
+                    grid[i][j]= new Tile(image);
     }
-
-    public int getGrid()
+    //sets the boundaries for the cave map which has a different border than the regualar box one
+    private void setBoundariesCave(String image)
     {
-        return grid;
+        for(int i=0; i<17;i++)
+            for (int j=0; j<17;j++)
+                if ((i==1 && (j<7 || j>9)) || (i==15 && (j<7 || j>9)) || j==0 || j==16 || j==6 || j==10)
+                    grid[i][j]= new Tile(image);
+    }
+    //sets the boundaries for the river map which has a different border than the regular box one
+    private void setBoundariesRiver(String image)
+    {
+        for(int i=0; i<17;i++)
+            for (int j=0; j<17;j++)
+               //PLACEHOLDER if ((i==1 && (j<7 || j>9)) || (i==15 && (j<7 || j>9)) || j==0 || j==16 || j==6 || j==10)
+                    grid[i][j]= new Tile(image);
+    }
+    //uses x and y to set a specific tile to a specific image/object
+    private void addObject(int x,int y, String image)
+    {
+        grid[x][y]= new Tile(image);
+    }
+    //returns a specific tile
+    public Tile getTile(int i,int j)
+    {
+        return Tile[i][j];
     }
 }
