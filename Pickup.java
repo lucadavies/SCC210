882f2e08b6ccc7c46f60448c585036cee2c30ed0
Javier Asua <asuajavier@gmail.com>
2019-01-23 17:00:10 +0000

    Pickup.java

--- Pickup.java
+++ Pickup.java
@@ -6,59 +6,109 @@ import java.util.*;
 import java.lang.Object;
 import org.jsfml.graphics.Drawable;
 import org.jsfml.graphics.RenderWindow;
-
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.function.BiConsumer;
 import java.util.function.IntConsumer;
+import java.nio.file.Paths;
+import org.jsfml.graphics.IntRect;
+import org.jsfml.graphics.Sprite;
+import org.jsfml.graphics.Texture;
 
-public class Pickup extends MovingEntity{
+public class Pickup extends Entity{
     
+    private Sprite img;
     
-    public enum pickUpType{
-        smallLaserGun;
-        superLaserGun;
-        bomb;
-        misile;
-        oldMirror;
-        boots;
-        alienMess;
-        vaccumCleaner;
-        frozenStone;
-        fastStopWatch;
-        slowStopWatch;
-        angleWings;
-        doubleBullet;
-        tripleBullet;
-        superBullet;
-    }
-    
-    public Pickup(int x,int y,int r,String textureFile,float width,float height,int lineNumber,pickUpType seed)
+    //Creates a list of pickUps.
+    public enum pickUpType
     {
-        this.seed = seed;
-        super(x,y,r,textureFile,width,height,lineNumber);
-        switch (seed){
-            case smallLaserGun:
-            case superLaserGun:
-            case bomb:
-            case misile:
-            case oldMirror:
-            case oldMirror:
-            case boots:
-            case alienMess:
-            case vaccumCleaner:
-            case frozenStone:
-            case fastStopWatch:
-            case slowStopWatch:
-            case angleWings:
-            case doubleBullet:
-            case tripleBullet:
-            case superBullet:
-                break;
-                
-        }
-        
-        
-        
-        
+       
+        smallLaserGun,        
+        superLaserGun,
+        bomb,
+        misile,
+        oldMirror,
+        boots,
+        alienMess,
+        vaccumCleaner,
+        frozenStone,
+        fastStopWatch,
+        slowStopWatch,
+        angleWings,
+        doubleBullet,
+        tripleBullet,
+        superBullet
     }
+    
+    //constructor for pickUps.
+    public Pickup(pickUpType pickup)
+    {
+        //this.pickUpType = pickUpType;
+        
+        switch (pickup){
+            case smallLaserGun:
+                this.setImgTexture("smallGun.png");
+                    break;
+            case superLaserGun:
+                this.setImgTexture("superLaserGun.png");
+                    break;
+            case bomb:
+                this.setImgTexture("Bomb.png");
+                    break;
+            case misile:
+                this.setImgTexture("Missile.png");
+                    break;
+            case oldMirror:
+                this.setImgTexture("oldMirror.png");
+                    break;
+            case boots:
+                this.setImgTexture("boots.png");
+                    break;
+            case alienMess:
+                this.setImgTexture("alienMess.png");
+                    break;
+            case vaccumCleaner:
+                this.setImgTexture("vaccumCleaner.png");
+                    break;
+            case frozenStone:
+                this.setImgTexture("frozen.png");
+                    break;
+            case fastStopWatch:
+                this.setImgTexture("fastStopWatch.png");
+                    break;
+            case slowStopWatch:
+                this.setImgTexture("slowStopWatch.png");
+                    break;
+            case angleWings:
+                this.setImgTexture("angleWings.png");
+                    break;
+            case doubleBullet:
+                this.setImgTexture("doubleBullet.png");
+                    break;
+            case tripleBullet:
+                this.setImgTexture("tripleBullet.png");
+                    break;
+            case superBullet:
+                this.setImgTexture("superBullet.png");
+                    break;
+                
+            }
+    }
+    
+    //Method to show the image texture copied from movingEntity.
+    public void setImgTexture(String texture) {
+            Texture imgTexture = new Texture();
+            try {
+                imgTexture.loadFromFile(Paths.get(texture));
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+                img = new Sprite(imgTexture);
+      }
+    
+     public void 
+        
+        
+        
+}
 
