55713972040e82df5a00f7800cf5a2c49577824d
Javier Asua <asuajavier@gmail.com>
2019-02-04 19:22:54 +0000

    added hitbox to walker

--- Walker.java
+++ Walker.java
@@ -11,13 +11,19 @@ import org.jsfml.graphics.RectangleShape;
 
 public class Walker extends Alien{
 
+    public static final int WALKER_X = 500;
+    public static final int WALKER_Y = 500;
+
+    public CollisionBox walkerHitBox = new CollisionBox(WALKER_X, WALKER_Y, 60, 60);
+
     public Walker(){
-      super(500,500,"art/enemy/Walker.png");
+      super(WALKER_X,WALKER_Y,"art/enemy/Walker.png");
       super.moveLeft();
       super.moveAwayFromCamera();
       super.moveRight();
       super.moveTowardCamera();
       super.setSpriteWithinSheet(1, 1);
+      walkerHitBox.setPosition(WALKER_X,WALKER_Y);
       //super.moveEnemy();
 
 
