6d0a580f493ccf666e028c20eeae05ea267c549c
Luca Davies <luca-davies@outlook.com>
2019-02-08 23:02:04 +0000

    Commented out unused methods in unused OutOfBounds.java
    Last commit broke the build... sorry...

--- OutOfBounds.java
+++ OutOfBounds.java
@@ -25,52 +25,52 @@ public class OutOfBounds {
 
 
     //set all points to 1
-    public void initialise(int[][] a){
-      for(int i=0;i<yMax;i++){
-        for(int j=0;j<xMax;j++){
-          playerArea[j][i] = 1;
+    public void initialise(int[][] a) {
+        for (int i = 0; i < yMax; i++) {
+            for (int j = 0; j < xMax; j++) {
+                playerArea[j][i] = 1;
+            }
         }
-      }
     }
 
 
     //set all out of bounds points to 0
-    public void accept(int x, int y, int w, int h){
-      for(int i=0;i<h;i++){
-        for(int j=0;j<w;j++){
-          playerArea[j+x][i+y] = 0;
+    public void accept(int x, int y, int w, int h) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                playerArea[j + x][i + y] = 0;
+            }
         }
-      }
     }
 
 
     //checks ahead to see if the player will be out of bounds and blocks movement appropriately
-    public void isOutOfBounds(int x, int y, int w, int h, int checkAhead){
-
-
-      for(int i=0;i<h;i++){
-          if(playerArea[x-checkAhead][i+y] == 0 || Player.getPlayerInstance().x == checkAhead){
-            Player.getPlayerInstance().canMoveLeft = false;
-          }else{
-            Player.getPlayerInstance().canMoveLeft = true;
-          }
-          if(playerArea[x+w+checkAhead][i+y] == 0 || Player.getPlayerInstance().x == 1020-checkAhead){
-            Player.getPlayerInstance().canMoveRight = false;
-          }else{
-            Player.getPlayerInstance().canMoveRight = true;
-          }
-      }
-      for(int i=0;i<w;i++){
-          if(playerArea[x+i][y-checkAhead] == 0 || Player.getPlayerInstance().y == checkAhead){
-            Player.getPlayerInstance().canMoveUp = false;
-          }else{
-            Player.getPlayerInstance().canMoveUp = true;
-          }
-          if(playerArea[x+i][h+y+checkAhead] == 0 || Player.getPlayerInstance().y == 1020-checkAhead){
-            Player.getPlayerInstance().canMoveDown = false;
-          }else{
-            Player.getPlayerInstance().canMoveDown = true;
-          }
-      }
+    public void isOutOfBounds(int x, int y, int w, int h, int checkAhead) {
+        /*
+        for (int i = 0; i < h; i++) {
+            if (playerArea[x - checkAhead][i + y] == 0 || Player.getPlayerInstance().x == checkAhead) {
+                Player.getPlayerInstance().canMoveLeft = false;
+            } else {
+                Player.getPlayerInstance().canMoveLeft = true;
+            }
+            if (playerArea[x + w + checkAhead][i + y] == 0 || Player.getPlayerInstance().x == 1020 - checkAhead) {
+                Player.getPlayerInstance().canMoveRight = false;
+            } else {
+                Player.getPlayerInstance().canMoveRight = true;
+            }
+        }
+        for (int i = 0; i < w; i++) {
+            if (playerArea[x + i][y - checkAhead] == 0 || Player.getPlayerInstance().y == checkAhead) {
+                Player.getPlayerInstance().canMoveUp = false;
+            } else {
+                Player.getPlayerInstance().canMoveUp = true;
+            }
+            if (playerArea[x + i][h + y + checkAhead] == 0 || Player.getPlayerInstance().y == 1020 - checkAhead) {
+                Player.getPlayerInstance().canMoveDown = false;
+            } else {
+                Player.getPlayerInstance().canMoveDown = true;
+            }
+        }
+        */
     }
 }
